135e492ab62a3e3f358c857d07ecce53978c42b3
Aykut Gokbulut <aykutgokbulut@outlook.com>
2026-02-28 19:11:56 +0300

    some files fixed

---
+++ c_examples/basics/scanf/example1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int sayi;
+
+    printf("bir sayi giriniz\n");
+    scanf("%d", &sayi);
+    printf("girilen sayi %d", sayi);
+
+    return 0;
+}
+
+/*
+int %d
+float %f
+char %c
+double %lf
+*/
